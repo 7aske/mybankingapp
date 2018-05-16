d5cadcf02fee7a96a6b7c62a9a3ec4c9aa54d200
Nikola Tasic <ntasic7@gmail.com>
2018-05-17 00:28:15 +0200

    added get all users api

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Users = require('../models/user');
 
 router.get('/', (req, res) => {
 	res.send('Hello API');
 });
-
+router.get('/users', (req, res) => {
+	Users.find({})
+		.then(result => {
+			res.status(200).send(result);
+		})
+		.catch(err => console.log(error));
+});
 module.exports = router;
