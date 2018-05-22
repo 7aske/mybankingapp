276806184c98594ecd4e7f42e36b3c16361947ca
Nikola Tasic <ntasic7@gmail.com>
2018-05-22 14:42:20 +0200

    new funds notification is working

--- public/js/balance.js
+++ public/js/balance.js
@@ -1,33 +1,44 @@
 let balance = document.querySelectorAll('.balance');
 balance.forEach(e => {
-    let firstQuery = true;
-    let accountNumber = e.value;
-    let url = new URL(window.location).origin + '/api/accounts/' + accountNumber;
-    let newBalance = 0;
-    let checkBalance = 0;
-    console.log(accountNumber, url)
-    function update() {
-        if (firstQuery) {
-            firstQuery = !firstQuery;
-            let request = new Request('get', url, null);
-            request.send().then(result => {
-                console.log(result);
-                checkBalance = JSON.parse(result.response).balance;
-            }).catch(err => console.log(err));
-        } else {
-            let request = new Request('get', url, null);
-            request.send().then(result => {
-                console.log(result);
-                newBalance = JSON.parse(result.response).balance;
-                if (newBalance == checkBalance) {
-                    console.log('No change');
-                } else {
-                    console.log('Funds recieved');
-                    checkBalance = newBalance
-                }
-
-            }).catch(err => console.log(err));
-        }
-    }
-    let interval = setInterval(update, 5000);
+	let firstQuery = true;
+	let accountNumber = e.value;
+	let url =
+		new URL(window.location).origin +
+		'/api/accounts/balance/' +
+		accountNumber;
+	let newBalance = 0;
+	let checkBalance = 0;
+	console.log(accountNumber, url);
+	function update() {
+		if (firstQuery) {
+			firstQuery = !firstQuery;
+			let request = new Request('get', url, null);
+			request
+				.send()
+				.then(result => {
+					checkBalance = JSON.parse(result.response).balance;
+				})
+				.catch(err => console.log(err));
+		} else {
+			let request = new Request('get', url, null);
+			request
+				.send()
+				.then(result => {
+					let response = JSON.parse(result.response);
+					newBalance = response.balance;
+					if (newBalance == checkBalance) {
+						console.log('No change');
+					} else {
+						console.log('Funds recieved');
+						checkBalance = newBalance;
+						window.location =
+							window.location.origin +
+							'/api/accounts/balance/update/' +
+							response.accountNumber;
+					}
+				})
+				.catch(err => console.log(err));
+		}
+	}
+	let interval = setInterval(update, 5000);
 });
--- routes/api.js
+++ routes/api.js
@@ -7,39 +7,67 @@ const Accounts = require('../models/account');
 router.get('/', (req, res) => {
 	res.send('Hello API');
 });
+router.get('/accounts/balance/update/:accNo', (req, res) => {
+	if (req.user) {
+		req.flash('success_msg', 'New funds recieved on ' + req.params.accNo);
+		res.redirect('/users/dashboard');
+	} else {
+		req.flash('error_msg', 'Unauthorized. Please log in.');
+		res.redirect('/users/login');
+	}
+});
 router.get('/users', (req, res) => {
-	Users.find({})
-		.then(result => {
-			res.status(200).send(result);
-		})
-		.catch(err => console.log(error));
+	if (req.user) {
+		Users.find({})
+			.then(result => {
+				res.status(200).send(result);
+			})
+			.catch(err => console.log(error));
+	} else {
+		req.flash('error_msg', 'Unauthorized. Please log in.');
+		res.redirect('/users/login');
+	}
+});
+router.get('/accounts/balance/:accNo', (req, res) => {
+	if (req.user) {
+		Accounts.findOne({ accountNumber: req.params.accNo })
+			.exec()
+			.then(result => {
+				res.status(200).send({
+					balance: result.balance,
+					accountNumber: result.accountNumber
+				});
+			})
+			.catch(err => console.log(err));
+	} else {
+		req.flash('error_msg', 'Unauthorized. Please log in.');
+		res.redirect('/users/login');
+	}
 });
 router.get('/accounts/:accNo', (req, res) => {
 	if (req.user) {
 		Accounts.findOne({ accountNumber: req.params.accNo })
 			.exec()
 			.then(result => {
-				let balance = result.balance
 				Users.findOne({ _id: result.owner })
 					.exec()
 					.then(result => {
 						res.send({
-							accountNumber: req.params.accNo,
 							firstName: result.firstName,
 							lastName: result.lastName,
 							company: result.company,
 							address: result.address,
 							city: result.city,
 							country: result.country,
-							phone: result.phone,
-							balance: balance
+							phone: result.phone
 						});
 					})
 					.catch(err => console.log(err));
 			})
 			.catch(err => console.log(err));
 	} else {
-		res.sendStatus(401);
+		req.flash('error_msg', 'Unauthorized. Please log in.');
+		res.redirect('/users/login');
 	}
 });
 module.exports = router;
