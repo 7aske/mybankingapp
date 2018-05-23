aa9f846a5aa462f5efbb777ff9d04fc230488a92
Nikola Tasic <ntasic7@gmail.com>
2018-05-23 18:40:38 +0200

    edited_script

--- push.cmd
+++ push.cmd
@@ -1,3 +1,3 @@
 @echo off
 set /p com="Commit name: "
-git add . & git commit -m %com% & git push -u origin master & git push heroku master
+git add . && git commit -m %com% && git push -u origin master & git push heroku master
